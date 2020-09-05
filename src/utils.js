7cb9eeb14c6fecbf46230ddf52ab51465ebd256d
Ilya Volkov <volkov-ilyha@ya.ru>
2020-09-05 15:52:07 +0300

    ℹ describes utility functions and constants

---
+++ src/utils.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * Возвращает случайное число в диапазоне
+ * `min` и `max`.
+ *
+ * @param {Number} min
+ * @param {Number} max
+ * @return {Number}
+ */
+module.exports.getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+/**
+ * Перетасовка массива по алгоритму
+ * Фишера—Йетса.
+ *
+ * Функция возвращает новый массив
+ *
+ * @param {Array} someArray
+ * @return {Array}
+ */
+module.exports.shuffle = (someArray) => {
+  for (let i = someArray.length - 1; i > 0; i--) {
+    const randomPosition = Math.floor(Math.random() * i);
+    [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
+  }
+
+  return someArray;
+};
