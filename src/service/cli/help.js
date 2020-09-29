5370ca89ec4cd4385aba359128afe16cb762e4d4
Ilya Volkov <volkov-ilyha@ya.ru>
2020-09-29 22:21:28 +0300

    🧷 fixes on the previous task

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -12,7 +12,8 @@ module.exports = {
       Команды:
       --version:            выводит номер версии
       --help:               печатает этот текст
-      --generate <count>    формирует файл mocks.json
+      --generate <count>:   формирует файл mocks.json
+      --server <port>:      запускает http-сервер
     `;
 
     console.log(chalk.gray(text));
