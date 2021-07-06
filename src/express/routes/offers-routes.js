6f116242420149065a4e3625e816865ada26f89f
Ilya Volkov <volkov-ilyha@ya.ru>
2021-07-06 12:27:01 +0300

    🌐 front-server: add data uploading in /offers/add route

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,12 +1,37 @@
 'use strict';
 
 const {Router} = require(`express`);
+const upload = require(`../middlewares/upload`);
 const api = require(`../api`).getAPI();
 
 const offersRouter = new Router();
 
-offersRouter.get(`/add`, (req, res) => res.render(`offers/new-ticket`));
+offersRouter.get(`/add`, async (req, res) => {
+  const categories = await api.getCategories();
+  res.render(`offers/new-ticket`, {categories});
+});
+
+offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
+  const {body, file} = req;
+  const offerData = {
+    category: Array.isArray(body.category) ? body.category : [body.category],
+    description: body.comment,
+    picture: file.filename,
+    title: body[`ticket-name`],
+    type: body.action,
+    sum: body.price,
+  };
+
+  try {
+    await api.createOffer(offerData);
+    res.redirect(`/my`);
+  } catch (e) {
+    res.redirect(`back`);
+  }
+});
+
 offersRouter.get(`/:id`, (req, res) => res.render(`offers/ticket`));
+
 offersRouter.get(`/edit/:id`, async (req, res) => {
   const {id} = req.params;
   const [offer, categories] = await Promise.all([
@@ -15,6 +40,7 @@ offersRouter.get(`/edit/:id`, async (req, res) => {
   ]);
   res.render(`offers/ticket-edit`, {offer, categories});
 });
+
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
 
 module.exports = offersRouter;
