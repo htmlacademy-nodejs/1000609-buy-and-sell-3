348bfe320e6161e6325a9d80e1c27b6ec1b0aa49
Ilya Volkov <volkov-ilyha@ya.ru>
2021-08-07 13:42:45 +0300

    🔄 updating data services

--- src/service/data-service/offer.js
+++ src/service/data-service/offer.js
@@ -1,43 +1,45 @@
 'use strict';
 
-const {nanoid} = require(`nanoid`);
-const {MAX_ID_LENGTH} = require(`../../constants`);
+const Alias = require(`../models/alias`);
 
 class OfferService {
-  constructor(offers) {
-    this._offers = offers;
+  constructor(sequelize) {
+    this._Offer = sequelize.models.Offer;
   }
 
-  create(offer) {
-    const newOffer = Object.assign({id: nanoid(MAX_ID_LENGTH), comments: []}, offer);
-
-    this._offers.push(newOffer);
-    return newOffer;
+  async create(offerData) {
+    const offer = await this._Offer.create(offerData);
+    await offer.addCategories(offerData.categories);
+    return offer.get();
   }
 
-  drop(id) {
-    const offer = this._offers.find((item) => item.id === id);
+  async drop(id) {
+    const deletedRows = await this._Offer.destroy({
+      where: {id}
+    });
+    return !!deletedRows;
+  }
 
-    if (!offer) {
-      return null;
+  async findAll(needComments) {
+    const include = [Alias.CATEGORIES];
+
+    if (needComments) {
+      include.push(Alias.COMMENTS);
     }
 
-    this._offers = this._offers.filter((item) => item.id !== id);
-    return offer;
-  }
-
-  findAll() {
-    return this._offers;
+    const offers = await this._Offer.findAll({include});
+    return offers.map((offer) => offer.get());
   }
 
   findOne(id) {
-    return this._offers.find((item) => item.id === id);
+    return this._Offer.findByPk(id, {include: [Alias.CATEGORIES]});
   }
 
-  update(id, offer) {
-    const oldOffer = this._offers.find((item) => item.id === id);
-
-    return Object.assign(oldOffer, offer);
+  async update(id, offer) {
+    const [affectedRows] = await this._Offer.update(offer, {
+      where: {id}
+    });
+    return !!affectedRows;
   }
 }
 
