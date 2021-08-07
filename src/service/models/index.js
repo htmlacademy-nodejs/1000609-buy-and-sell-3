bf0d6a97d2935eee65b009ec885044cc51d79291
Ilya Volkov <volkov-ilyha@ya.ru>
2021-08-07 12:20:04 +0300

    ↕ creating associations between models

---
+++ src/service/models/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const {Model} = require(`sequelize`);
+const Alias = require(`./alias`);
+const defineCategory = require(`./category`);
+const defineComment = require(`./comment`);
+const defineOffer = require(`./offer`);
+
+class OfferCategory extends Model {}
+
+const define = (sequelize) => {
+  const Category = defineCategory(sequelize);
+  const Comment = defineComment(sequelize);
+  const Offer = defineOffer(sequelize);
+
+  Offer.hasMany(Comment, {as: Alias.COMMENTS, foreignKey: `offerId`});
+  Comment.belongsTo(Offer, {foreignKey: `offerId`});
+
+  OfferCategory.init({}, {sequelize});
+
+  Offer.belongsToMany(Category, {through: OfferCategory, as: Alias.CATEGORIES});
+  Category.belongsToMany(Offer, {through: OfferCategory, as: Alias.OFFERS});
+  Category.hasMany(OfferCategory, {as: Alias.OFFER_CATEGORIES});
+
+  return {Category, Comment, Offer, OfferCategory};
+};
+
+module.exports = define;
