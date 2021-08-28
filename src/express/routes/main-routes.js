eff5060d4bdd1158f6b12605433397256f06c851
Ilya Volkov <volkov-ilyha@ya.ru>
2021-08-28 15:51:09 +0300

    ➕ front-server: add pagination

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,11 +3,19 @@
 const {Router} = require(`express`);
 const api = require(`../api`).getAPI();
 
+const OFFERS_PER_PAGE = 8;
+
 const mainRouter = new Router();
 
 mainRouter.get(`/`, async (req, res) => {
-  const [offers, categories] = await Promise.all([api.getOffers(), api.getCategories(true)]);
-  res.render(`main`, {offers, categories});
+  let {page = 1} = req.query;
+  page = +page;
+  const limit = OFFERS_PER_PAGE;
+  const offset = (page - 1) * OFFERS_PER_PAGE;
+
+  const [{count, offers}, categories] = await Promise.all([api.getOffers({limit, offset}), api.getCategories(true)]);
+  const totalPages = Math.ceil(count / OFFERS_PER_PAGE);
+  res.render(`main`, {offers, categories, page, totalPages});
 });
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
