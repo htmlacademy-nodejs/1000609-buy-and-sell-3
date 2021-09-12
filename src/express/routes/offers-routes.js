57684aace1f21a8ee4af71fad88f01b53c053abe
Ilya Volkov <volkov-ilyha@ya.ru>
2021-09-12 12:12:07 +0300

    🌐 front-server: add data uploading in /offers/edit/:id route

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -7,6 +7,14 @@ const api = require(`../api`).getAPI();
 
 const offersRouter = new Router();
 
+const getEditOfferData = async (offerId) => {
+  const [offer, categories] = await Promise.all([
+    api.getOffer(offerId),
+    api.getCategories()
+  ]);
+  return [offer, categories];
+};
+
 offersRouter.get(`/add`, async (req, res) => {
   const categories = await api.getCategories();
   res.render(`offers/new-ticket`, {categories});
@@ -17,7 +25,7 @@ offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
   const offerData = {
     categories: Array.isArray(body.category) ? body.category : [body.category],
     description: body.comment,
-    picture: file && file.filename ? file.filename : null,
+    picture: file ? file.filename : ``,
     title: body[`ticket-name`],
     type: body.action,
     sum: body.price,
@@ -42,11 +50,30 @@ offersRouter.get(`/:id`, async (req, res) => {
 
 offersRouter.get(`/edit/:id`, async (req, res) => {
   const {id} = req.params;
-  const [offer, categories] = await Promise.all([
-    api.getOffer(id),
-    api.getCategories()
-  ]);
-  res.render(`offers/ticket-edit`, {offer, categories});
+  const [offer, categories] = await getEditOfferData(id);
+  res.render(`offers/ticket-edit`, {id, offer, categories});
+});
+
+offersRouter.post(`/edit/:id`, upload.single(`avatar`), async (req, res) => {
+  const {body, file} = req;
+  const {id} = req.params;
+  const offerData = {
+    picture: file ? file.filename : body[`old-image`],
+    sum: body.price,
+    type: body.action,
+    description: body.comment,
+    title: body[`ticket-name`],
+    categories: Array.isArray(body.category) ? body.category : [body.category]
+  };
+
+  try {
+    await api.editOffer(id, offerData);
+    res.redirect(`/my`);
+  } catch (err) {
+    const validationMessages = prepareErrors(err);
+    const [offer, categories] = await getEditOfferData(id);
+    res.render(`offers/ticket-edit`, {id, offer, categories, validationMessages});
+  }
 });
 
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
