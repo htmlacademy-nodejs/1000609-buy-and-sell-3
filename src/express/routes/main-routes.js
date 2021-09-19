4ad552eb14a271a6b55d3088507966cf4ea56302
Ilya Volkov <volkov-ilyha@ya.ru>
2021-09-19 17:53:59 +0300

    🌐 front-server: add data loading in /register route

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const {Router} = require(`express`);
+const upload = require(`../middlewares/upload`);
+const {prepareErrors} = require(`../../utils`);
 const api = require(`../api`).getAPI();
 
 const OFFERS_PER_PAGE = 8;
@@ -17,7 +19,28 @@ mainRouter.get(`/`, async (req, res) => {
   const totalPages = Math.ceil(count / OFFERS_PER_PAGE);
   res.render(`main`, {offers, categories, page, totalPages});
 });
+
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
+
+mainRouter.post(`/register`, upload.single(`avatar`), async (req, res) => {
+  const {body, file} = req;
+  const userData = {
+    avatar: file ? file.filename : ``,
+    name: body[`user-name`],
+    email: body[`user-email`],
+    password: body[`user-password`],
+    passwordRepeated: body[`user-password-again`]
+  };
+
+  try {
+    await api.createUser(userData);
+    res.redirect(`/login`);
+  } catch (err) {
+    const validationMessages = prepareErrors(err);
+    res.render(`sign-up`, {validationMessages});
+  }
+});
+
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, async (req, res) => {
   const {search} = req.query;
